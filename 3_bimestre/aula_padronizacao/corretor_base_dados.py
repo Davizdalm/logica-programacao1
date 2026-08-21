cf2f394025ac5a3a09a5b286d3213726a5f861c1
Davizdalm <davi.almeida25@eaportal.org>
2026-08-21 13:57:31 +0000

    corrretor

---
+++ 3_bimestre/aula_padronizacao/corretor_base_dados.py
@@ -0,0 +1,25 @@
+brutos = [
+ "joao.souza@EMAIL.com ",
+ "    RUA DAS FLORES, No 123",
+ " 000.111.222-33",
+ "CARLOS.ROCHA@ESCOLA.ORG    ",
+ "  AV. CENTRAL, No 450   "
+]
+
+dados_limpos = []
+
+for item in brutos: 
+    texto = item.strip()
+    if "@" in texto:
+        texto = texto.lower()
+    else:
+        texto = texto.replace("No", "Número")
+
+        texto = texto.replace(".", "").replace("-", "")
+    
+    dados_limpos.append(texto)
+print("=============================================")
+print("     BASE DE DADOS TRATADA E SANITIZADA      ")
+print("=============================================")
+for dado in dados_limpos:
+    print("-", dado)
