5985f42ecac68ffb634b3ddacbab98531b05130f
Davizdalm <davi.almeida25@eaportal.org>
2026-08-27 12:13:12 +0000

    atividades em sala

---
+++ 3_bimestre/revisao_av1_formatador.py
@@ -0,0 +1,26 @@
+def formatar_citacao(nome_completo):
+    partes = nome_completo.strip().split()
+
+    sobrenome = partes[-1].upper()
+
+    primeiro_nome = " ".join(partes[:-1])
+
+    return sobrenome + ", " + primeiro_nome
+
+resultado1 = formatar_citacao("Carlos Eduardo Andrade")
+print(resultado1)
+
+#--------------------------------------------------------#
+
+def gerar_codigo(ano, cpf):
+    cpf_limpo = cpf.strip()
+    tres_digitos = cpf_limpo[0:3]
+    return "ALU-" + str(ano) + "-" +tres_digitos
+
+    
+
+
+
+
+resultado2 = gerar_codigo("2026", "154.607.999-86")
+print(resultado2) 
